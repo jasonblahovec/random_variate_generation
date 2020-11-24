06b7941763391fcf5697a9049e3964c2eaece8fa
jasonblahovec <74939208+jasonblahovec@users.noreply.github.com>
2020-11-23 20:37:14 -0500

    Update generate_random_variables.py

--- generate_random_variables.py
+++ generate_random_variables.py
@@ -4,18 +4,21 @@ import math
 np.set_printoptions(suppress=True)
 
 class generate_uniform():
-    def __init__(self, n = 1000, seed = 412, mult = 16807, mod = (2**31)-1):
+    def __init__(self, n = 1000, seed = 412, a = 0, b = 1, mult = 16807, mod = (2**31)-1):
         self.n = n
         self.seed = seed
         self.mult = mult
         self.mod = mod
+        
+        self.a = a 
+        self.b = b
         
         self.get_observations()
         
     def get_observations(self):
         self.observations = np.ones(self.n)*self.seed
         for i in range(1,len(self.observations)):
-            self.observations[i] = (self.observations[i-1]*self.mult)%self.mod
+            self.observations[i] = (self.b-self.a)*((self.observations[i-1]*self.mult)%self.mod)/self.b
   
         self.observations = (self.observations/self.mod)
     
@@ -32,8 +35,8 @@ class generate_normal():
         self.get_bm_normals()
 
     def get_bm_normals(self):
-        u1 = generate_uniforms(n = self.n, seed = self.seed, mult = self.unif_mult, mod = self.unif_mod)
-        u2 = generate_uniforms(n = self.n, seed = self.seed, mult = self.unif_mult, mod = self.unif_mod)
+        u1 = generate_uniforms(n = self.n, seed = self.seed, a = 0, b = 1 , mult = self.unif_mult, mod = self.unif_mod)
+        u2 = generate_uniforms(n = self.n, seed = self.seed, a = 0, b = 1 , mult = self.unif_mult, mod = self.unif_mod)
         
         self.observations = ((-2*np.log(u1.observations))**.5)*(np.cos(2*np.pi*u2.observations))
  
@@ -49,7 +52,7 @@ class generate_bernoulli():
         self.get_bernoulli()
 
     def get_bernoulli(self):
-        u1 = generate_uniforms(n = self.n, seed = self.seed, mult = self.unif_mult, mod = self.unif_mod)
+        u1 = generate_uniforms(n = self.n, seed = self.seed, a = 0, b = 1 ,  mult = self.unif_mult, mod = self.unif_mod)
         
         self.observations = np.where(u1.observations > self.p, 0, 1)   
         
@@ -65,7 +68,7 @@ class generate_exponential():
         self.get_exponential()
         
     def get_exponential(self):
-        u1 = generate_uniforms(n = self.n, seed = self.seed, mult = self.unif_mult, mod = self.unif_mod)
+        u1 = generate_uniforms(n = self.n, seed = self.seed, a = 0, b = 1 ,  mult = self.unif_mult, mod = self.unif_mod)
         self.observations = (-1/self._lambda)*np.log(1-u1.observations)
         
 class generate_geometric():
@@ -80,5 +83,5 @@ class generate_geometric():
         self.get_geometrics()
         
     def get_geometrics(self):
-        u1 = generate_uniforms(n = self.n, seed = self.seed, mult = self.unif_mult, mod = self.unif_mod)
+        u1 = generate_uniforms(n = self.n, seed = self.seed, a = 0, b = 1 ,  mult = self.unif_mult, mod = self.unif_mod)
         self.observations = np.ceil(np.log(u1.observations)/np.log(1-self.p))
